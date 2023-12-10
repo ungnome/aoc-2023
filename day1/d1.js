5d6edcd9e53d97b3e2f560cb56e60cc99bf6fd46
ungnome <73322863+ungnome@users.noreply.github.com>
2023-12-10 00:37:04 -0500

    d1p2 solution

--- day1/d1.js
+++ day1/d1.js
@@ -1,6 +1,6 @@
 import { open } from 'node:fs/promises';
 
-async function main() {
+async function part1() {
 	const calibrationInput = await open('input.txt');
 	const calibrationData = [];
 	let calibrationTotal = 0;
@@ -13,7 +13,7 @@ async function main() {
 		calibrationTotal += calibration;
 	}
 
-	console.log(calibrationTotal);
+	return calibrationTotal;
 }
 
 function getCalibrationFromLine(line) {
@@ -28,4 +28,88 @@ function getCalibrationFromLine(line) {
 
 	return !digit2 ? Number('' + digit1 + digit1) : Number('' + digit1 + digit2);
 }
-main();
+
+async function part2() {
+	const calibrationInput = await open('input.txt');
+	const calibrationData = [];
+	let calibrationTotal = 0;
+	const wordsToDigits = {
+		zero: 0,
+		one: 1,
+		two: 2,
+		three: 3,
+		four: 4,
+		five: 5,
+		six: 6,
+		seven: 7,
+		eight: 8,
+		nine: 9,
+	};
+
+	// loop through each line
+	for await (const line of calibrationInput.readLines()) {
+		let digit1;
+		let digit2;
+
+		// search forward for first digit, one character at a time
+		let foundFirst = false;
+		for (let i = 0; i < line.length; i++) {
+			if (foundFirst) {
+				break;
+			}
+
+			// check if character is a digit
+			if (isDigit(line[i])) {
+				digit1 = line[i];
+				foundFirst = true;
+			} else {
+				// check if current position in line starts with a spelled out number
+				for (const word in wordsToDigits) {
+					if (line.startsWith(word, i)) {
+						digit1 = wordsToDigits[word];
+						foundFirst = true;
+					}
+				}
+			}
+		}
+
+		// search backward for second digit, one character at a time
+		let foundSecond = false;
+		for (let i = line.length - 1; i >= 0; i--) {
+			if (foundSecond) {
+				break;
+			}
+
+			// check if character is a digit
+			if (isDigit(line[i])) {
+				digit2 = line[i];
+				foundSecond = true;
+			} else {
+				// check if current position in line starts with a spelled out number
+				for (const word in wordsToDigits) {
+					if (line.startsWith(word, i)) {
+						digit2 = wordsToDigits[word];
+						foundSecond = true;
+					}
+				}
+			}
+		}
+
+		// add number to calibrationData
+		calibrationData.push(Number(`${digit1}${digit2}`));
+	}
+
+	// calculate total
+	for (const calibration of calibrationData) {
+		calibrationTotal += calibration;
+	}
+
+	return calibrationTotal;
+}
+
+function isDigit(str) {
+	return Number.isInteger(Number(str));
+}
+
+console.log(`part1: ${await part1()}`);
+console.log(`part2: ${await part2()}`);
