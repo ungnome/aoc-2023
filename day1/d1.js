7dad8800dde2212dec85f8cd67698d7d6c1b3f83
ungnome <73322863+ungnome@users.noreply.github.com>
2023-12-09 21:59:47 -0500

    day1 solution

--- day1/d1.js
+++ day1/d1.js
@@ -1,3 +1,31 @@
-function testFunction() {
-	return true;
+import { open } from 'node:fs/promises';
+
+async function main() {
+	const calibrationInput = await open('input.txt');
+	const calibrationData = [];
+	let calibrationTotal = 0;
+
+	for await (const line of calibrationInput.readLines()) {
+		calibrationData.push(getCalibrationFromLine(line));
+	}
+
+	for (const calibration of calibrationData) {
+		calibrationTotal += calibration;
+	}
+
+	console.log(calibrationTotal);
 }
+
+function getCalibrationFromLine(line) {
+	let digit1;
+	let digit2;
+
+	for (const character of line) {
+		if (Number.isInteger(Number(character))) {
+			!digit1 ? (digit1 = character) : (digit2 = character);
+		}
+	}
+
+	return !digit2 ? Number('' + digit1 + digit1) : Number('' + digit1 + digit2);
+}
+main();
